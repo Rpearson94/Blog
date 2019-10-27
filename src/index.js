3668c615312a0f0a8000a9c95430cc627921e21a
Ryan Pearson <rpearson1994@icloud.com>
2019-10-27 12:19:33 -0400

    Updated App to view posts.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css'; // import Bootstrap CSS library
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+// With React Router, we no long need a centralized App component anymore
+// import App from './components/App';
+import rootReducer from './reducers';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import PostsIndex from './containers/posts_index';
+import ReduxPromise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(ReduxPromise))
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Route path='/api/posts' component={PostsIndex} />
+        <Route exact path='/' component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
